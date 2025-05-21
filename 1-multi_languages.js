c9e7d7598491b6116e46d252f4b88f673a465084
Fiona Wambua <fionawambua20@gmail.com>
2025-05-21 16:06:57 +0300

    commit the changes

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let firstline ='C is fun';
+let secondline = 'Python is cool';
+let thirdline = 'JavaScript is amazing';
+
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
